15946e1eae20d2163420b2fea906097ff8fba213
adengz <adengz@gmail.com>
2020-05-21 21:05:13 -0700

    Anonymize tests

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,5 +1,5 @@
 from datetime import datetime, timedelta
-from fleeter.models import User, Fleet, Follow
+from fleeter.models import User, Fleet
 
 
 def test_follow(session):
@@ -12,8 +12,8 @@ def test_follow(session):
     assert user2.followers.all() == []
 
     user1.follow(user2)
-    assert user1.following.all() == [user2]
-    assert user2.followers.all() == [user1]
+    assert user2 in user1.following.all()
+    assert user1 in user2.followers.all()
 
 
 def test_unfollow(session):
@@ -21,8 +21,7 @@ def test_unfollow(session):
     user2 = User(username='jane')
     session.add_all([user1, user2])
     session.commit()
-    follow = Follow(follower_id=user1.id, followee_id=user2.id)
-    session.add(follow)
+    user1.following.append(user2)
     session.commit()
 
     assert user1.is_following(user2)
@@ -49,21 +48,29 @@ def test_user_get_own_fleets(session):
 
 
 def test_user_get_newsfeed(session):
-    me = User(username='me')
-    my_fleet = Fleet(post='DO NOT TAKE THAT HELI Kobe', user=me,
-                     created_at=datetime(2020, 1, 26, 9, 00, 00, 0))
-    ariana = User(username='ArianaGrande')
-    ariana_fleet = Fleet(post='sending love. be safe.', user=ariana,
-                         created_at=datetime(2020, 3, 12, 15, 12, 00, 0))
-    trump = User(username='realDonaldTrump')
-    trump_fleet = Fleet(post='Despite the constant negative press covfefe',
-                        user=trump,
-                        created_at=datetime(2017, 5, 31, 0, 6, 00, 0))
-    session.add_all([me, ariana, trump])
+    users = {}
+    for name in ['mike', 'susan', 'david', 'karen']:
+        users[name] = User(username=name)
+    session.add_all(users.values())
     session.commit()
-    follow = Follow(follower_id=me.id, followee_id=ariana.id)
-    session.add(follow)
+    users['susan'].following.append(users['mike'])
+    users['susan'].following.append(users['karen'])
+    users['david'].following.append(users['mike'])
+    users['karen'].following.append(users['mike'])
+    users['karen'].following.append(users['david'])
+    users['karen'].following.append(users['susan'])
+    now = datetime.now()
+    f1 = Fleet(post='fleet by mike', user=users['mike'],
+               created_at=(now + timedelta(seconds=1)))
+    f2 = Fleet(post='fleet by david', user=users['david'],
+               created_at=(now + timedelta(seconds=2)))
+    f3 = Fleet(post='fleet by karen', user=users['karen'],
+               created_at=(now + timedelta(seconds=3)))
+    f4 = Fleet(post='fleet by susan', user=users['susan'],
+               created_at=(now + timedelta(seconds=4)))
     session.commit()
 
-    news_feed = me.get_fleets(following=True)
-    assert news_feed.all() == [ariana_fleet, my_fleet]
+    assert users['mike'].get_fleets(following=True).all() == [f1]
+    assert users['susan'].get_fleets(following=True).all() == [f4, f3, f1]
+    assert users['david'].get_fleets(following=True).all() == [f2, f1]
+    assert users['karen'].get_fleets(following=True).all() == [f4, f3, f2, f1]
