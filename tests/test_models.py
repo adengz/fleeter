877143043a5ef9a7cbbd5caf79dcbee706c54ad7
adengz <adengz@gmail.com>
2020-05-26 23:58:04 -0700

    Add tests for to dict methods

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,7 +1,33 @@
 from fleeter.models import User, Fleet
 
 
-def test_follow(session):
+def test_user_to_dict(session):
+    lamar = User.query.filter_by(username='Lamar Davis').first()
+    lamar_dict = lamar.to_dict()
+    assert lamar_dict['id'] == 3
+    assert lamar_dict['username'] == 'Lamar Davis'
+    assert lamar_dict['fleets'] == 6
+    assert lamar_dict['following'] == 4
+    assert lamar_dict['followers'] == 9
+
+    jimmy = User.query.filter_by(username='Jimmy De Santa').first()
+    jimmy_dict = jimmy.to_dict()
+    assert jimmy_dict['id'] == 14
+    assert jimmy_dict['username'] == 'Jimmy De Santa'
+    assert jimmy_dict['fleets'] == 20
+    assert jimmy_dict['following'] == 3
+    assert jimmy_dict['followers'] == 3
+
+    ron = User.query.filter_by(username='Ron Jakowski').first()
+    ron_dict = ron.to_dict()
+    assert ron_dict['id'] == 26
+    assert ron_dict['username'] == 'Ron Jakowski'
+    assert ron_dict['fleets'] == 0
+    assert ron_dict['following'] == 1
+    assert ron_dict['followers'] == 1
+
+
+def test_user_follow(session):
     franklin = User.query.filter_by(username='Franklin Clinton').first()
     lamar = User.query.filter_by(username='Lamar Davis').first()
     assert lamar not in franklin.following.all()
@@ -19,7 +45,7 @@ def test_follow(session):
     assert michael in jimmy.followers.all()
 
 
-def test_unfollow(session):
+def test_user_unfollow(session):
     lamar = User.query.filter_by(username='Lamar Davis').first()
     stretch = User.query.filter_by(username='Harold Stretch Joseph').first()
     assert lamar.is_following(stretch)
@@ -77,3 +103,21 @@ def test_user_get_newsfeed(session):
         all_kyle_fleets.extend(Fleet.query.filter_by(user_id=user.id))
     all_kyle_fleets.sort(key=lambda f: f.created_at, reverse=True)
     assert all_kyle_fleets == kyle_newsfeed
+
+
+def test_fleet_to_dict(session):
+    jewel_post = 'NO!!!! My favorite jewelry store got robbed!'
+    jewel = Fleet.query.get(29)
+    jewel_dict = jewel.to_dict()
+    assert jewel_dict['id'] == 29
+    assert jewel_dict['post'] == jewel_post
+    assert jewel_dict['username'] == 'Amanda De Santa'
+
+    ud_post = 'How the hell did the Union Depository get broken into? ' \
+              'I thought it was supposed to be impenetrable! ' \
+              'What\'s this going to do to the dollar?'
+    ud = Fleet.query.get(134)
+    ud_dict = ud.to_dict()
+    assert ud_dict['id'] == 134
+    assert ud_dict['post'] == ud_post
+    assert ud_dict['username'] == 'Hayden Dubose'
